4c81b656b7c5935b92490e7b81fc3091599495cb
Chandramowli J <chamowstudios@outlook.com>
2017-05-07 18:19:45 +0530

    Add comments

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,32 +4,37 @@ import random
 import os
 
 class Tic_Tac_Toe(object):
-
+	#used to print the status of the game
 	def PrintTable(self, position, turn):
 		print("\n")
 		print("\n")
+		#position tells whose turn currently is
 		position -= 1
 		if turn == 1:
 			self.table[position] = "X"
 		elif turn == -1:
 			self.table[position] = "O"
-		for i in range(0,(self.no_of_blocks)):
+		#formatting the table
+		for i in range(0,(self.no_of_rows)):
 			print('\t\t\t', end='')
-			for j in range(0,(self.no_of_blocks)):
-				if self.table[(i*self.no_of_blocks)+j] == "X" or \
-				self.table[(i*self.no_of_blocks)+j] == "O":
+			for j in range(0,(self.no_of_rows)):
+				if self.table[(i*self.no_of_rows)+j] == "X" or \
+				self.table[(i*self.no_of_rows)+j] == "O":
 					print(" |  ", end='')
-					print(self.table[(i*(self.no_of_blocks))+j], end = '')
+					print(self.table[(i*(self.no_of_rows))+j], end = '')
 				else:	
 					print(" | ", end='')
-					print(self.table[(i*(self.no_of_blocks))+j], end = '')
+					print(self.table[(i*(self.no_of_rows))+j], end = '')
 			print(" |\n")
+		#checking for winner
 		self.game_over = self.CheckWinner()
 		
 
 	def Play(self):
+		#to avoid problems in formatting eg. 9 requires 1 space but 12 requires 2 spaces
+		#table looks hapazard so adding a 0 before numbers from 1 - 9
 		self.table = []
-		for i in range (0,(self.no_of_blocks)*(self.no_of_blocks)):
+		for i in range (0,(self.no_of_rows)*(self.no_of_rows)):
 			if i <= 8:
 				self.table.append("0" + str(i+1))
 			else:
@@ -38,25 +43,25 @@ class Tic_Tac_Toe(object):
 		turn = 0
 		self.PrintTable(position, turn)
 
-
+		#to make player1's symbol as "X" always
 		if self.player2_symbol == "X":
 			self.player1_symbol, self.player2_symbol = self.player2_symbol, self.player1_symbol
 			self.player1_name, self.player2_name = self.player2_name, self.player1_name
 		counter = 0
 		turn = int(1)
-		while(counter < (self.no_of_blocks)*(self.no_of_blocks)):
+		while(counter < (self.no_of_rows)*(self.no_of_rows)):
 			counter += 1
 			if turn == 1:
-				print("\n\t\t{0} it's your turn. Enter any position (1-{1}): " .format(self.player1_name, self.no_of_blocks * self.no_of_blocks), end = '')
+				print("\n\t\t{0} it's your turn. Enter any position (1-{1}): " .format(self.player1_name, self.no_of_rows * self.no_of_rows), end = '')
 			else:
-				print("\n\t\t{0} it's your turn. Enter any position (1-{1}): " .format(self.player2_name, self.no_of_blocks * self.no_of_blocks), end = '')
+				print("\n\t\t{0} it's your turn. Enter any position (1-{1}): " .format(self.player2_name, self.no_of_rows * self.no_of_rows), end = '')
  
 			position = int(input())
 			
 
-
+			#checking for correct input
 			if (position >= 1 and
-				position <= ((self.no_of_blocks)*(self.no_of_blocks))):
+				position <= ((self.no_of_rows)*(self.no_of_rows))):
 				if(self.table[position-1] != "X" and
 					self.table[position-1] != "O"):
 					self.PrintTable(position, turn)
@@ -73,7 +78,7 @@ class Tic_Tac_Toe(object):
 					continue
 			else:
 				counter -= 1
-				print("\n\t\tPlease enter a position in the range (1-{0})!".format(self.no_of_blocks * self.no_of_blocks))
+				print("\n\t\tPlease enter a position in the range (1-{0})!".format(self.no_of_rows * self.no_of_rows))
 				continue
 			turn *= (-1)
 		if self.game_over == "-1":
@@ -83,9 +88,9 @@ class Tic_Tac_Toe(object):
 	def New_Game(self):
 		print("\033[1;31m\n\n\t\t\t TIC-TAC-TOE !!\n\t\t\t --- --- ---")
 		while(1):
-			self.no_of_blocks = int(input("\n\t\tEnter number of blocks(2 - 9): "))
-			if self.no_of_blocks > 9 or self.no_of_blocks < 2:
-				print("\n\t\t Block number is restricted to range [2,9]!")
+			self.no_of_rows = int(input("\n\t\tEnter number of rows(2 - 9): "))
+			if self.no_of_rows > 9 or self.no_of_rows < 2:
+				print("\n\t\t Row number is restricted to range [2,9]!")
 			else:
 				break
 		self.player1_name = input("\n\n\t\tEnter the name of Player1: ")
@@ -118,11 +123,12 @@ class Tic_Tac_Toe(object):
 			self.player1_symbol = "O"
 		self.Play()
 
-	
+	#to check whether game has ended or not
 	def CheckWinner(self):
 		is_game_over = True
-		for i in range (1, self.no_of_blocks):
-			if self.table[(i*self.no_of_blocks)+i] != self.table[((i-1)*self.no_of_blocks)+(i-1)]:
+		#checking primary diagonal
+		for i in range (1, self.no_of_rows):
+			if self.table[(i*self.no_of_rows)+i] != self.table[((i-1)*self.no_of_rows)+(i-1)]:
 				is_game_over = False
 				break
 		if is_game_over == True:
@@ -130,32 +136,34 @@ class Tic_Tac_Toe(object):
 			
 
 		is_game_over = True
-		for i in range (1, self.no_of_blocks):
-			if self.table[(i*self.no_of_blocks)+((self.no_of_blocks-1)-i)] != self.table[((i-1)*(self.no_of_blocks))+((self.no_of_blocks-1)-(i-1))]:
+		#checking secondary diagonal
+		for i in range (1, self.no_of_rows):
+			if self.table[(i*self.no_of_rows)+((self.no_of_rows-1)-i)] != self.table[((i-1)*(self.no_of_rows))+((self.no_of_rows-1)-(i-1))]:
 				is_game_over = False
 				break
 		if is_game_over == True:
-			return self.table[(self.no_of_blocks-1)]
+			return self.table[(self.no_of_rows-1)]
 
 		is_game_over = True
-		for i in range (0, self.no_of_blocks):
+		#checking rows
+		for i in range (0, self.no_of_rows):
 			is_game_over = True
-			for j in range (1, self.no_of_blocks):
-				if self.table[(i*(self.no_of_blocks))+j] != self.table[(i*(self.no_of_blocks))+j-1]:
+			for j in range (1, self.no_of_rows):
+				if self.table[(i*(self.no_of_rows))+j] != self.table[(i*(self.no_of_rows))+j-1]:
 					is_game_over = False
 					break
 			if is_game_over == True:
-				return self.table[(i*(self.no_of_blocks))]
-
-		for i in range (0, (self.no_of_blocks)):
+				return self.table[(i*(self.no_of_rows))]
+		#checking columns
+		for i in range (0, (self.no_of_rows)):
 			is_game_over = True
-			for j in range (1, (self.no_of_blocks)):
-				if self.table[(j*(self.no_of_blocks))+i] != self.table[((j-1)*(self.no_of_blocks))+i]:
+			for j in range (1, (self.no_of_rows)):
+				if self.table[(j*(self.no_of_rows))+i] != self.table[((j-1)*(self.no_of_rows))+i]:
 					is_game_over = False
 					break
 			if is_game_over == True:
 				return self.table[(i)]
-
+		#if winner cannot be decided at this stage
 		return "-1"
 
 while(1):
